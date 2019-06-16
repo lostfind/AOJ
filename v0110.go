6b1ca9ec5d9171dd4a7f48a0645e3530c6e22b38
lostfind <kpdzzang@gmail.com>
2019-06-16 18:44:14 +0900

    WA

--- v0110.go
+++ v0110.go
@@ -1,21 +1,24 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strconv"
 	"strings"
 )
 
+var scanner = bufio.NewScanner(os.Stdin)
+
 func main() {
-	var input string
 	var left, right, awnser string
 	var splits []string
 
-	for {
-		i, _ := fmt.Scanf("%s", &input)
+	for scanner.Scan() {
+		input := scanner.Text()
 
-		if i != 1 {
-			break
+		if input == "" {
+			return
 		}
 
 		splits = strings.Split(input, "+")
@@ -26,15 +29,42 @@ func main() {
 		right = splits[0]
 		awnser = splits[1]
 
-		var rpLeft string
-		for i := 0; i < 10; i++ {
-
-			rpLeft = strings.Replace(left, "X", strconv.Itoa(i), -1)
-			// var l, r, a int
-			fmt.Println(rpLeft)
-
+		startIndex := 0
+		if len(left) > 1 && string(left[0]) == "X" {
+			startIndex = 1
+		}
+		if len(right) > 1 && string(right[0]) == "X" {
+			startIndex = 1
+		}
+		if len(awnser) > 1 && string(awnser[0]) == "X" {
+			startIndex = 1
 		}
 
+		isNa := true
+
+		for i := startIndex; i < 10; i++ {
+			rpLeft := strings.Replace(left, "X", strconv.Itoa(i), -1)
+			rpRight := strings.Replace(right, "X", strconv.Itoa(i), -1)
+			rpAwnser := strings.Replace(awnser, "X", strconv.Itoa(i), -1)
+
+			intLeft, _ := strconv.Atoi(rpLeft)
+			intRight, _ := strconv.Atoi(rpRight)
+			intAwnser, _ := strconv.Atoi(rpAwnser)
+
+			// fmt.Println(intLeft, intRight, intAwnser)
+			if (intLeft + intRight) == intAwnser {
+				fmt.Println(i)
+				isNa = false
+				break
+			}
+		}
+
+		if isNa == true {
+			fmt.Println("NA")
+		}
 	}
-	fmt.Println("AAAA", left, right, awnser)
 }
+
+// 123+4X6=X79
+// 12X+4X6=X79
+// XX22+89=X2XX
