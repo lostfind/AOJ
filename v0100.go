4a33073a603a35eaa4dc944456a5125bfde104ff
lostfind <kpdzzang@gmail.com>
2019-06-16 17:33:45 +0900

    modify map to slice

--- v0100.go
+++ v0100.go
@@ -1,31 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var scanner = bufio.NewScanner(os.Stdin)
 
 func main() {
-	for {
-		var n, e, q1, q2 int
-		fmt.Scanf("%d", &n)
-
+	for scanner.Scan() {
+		n, _ := strconv.Atoi(scanner.Text())
 		if n == 0 {
 			break
 		}
 
-		result := map[int]int{}
+		sales := make([]int, 4000)
+		members := make([]int, 0)
+		memberCheck := make(map[int]bool)
+
 		for i := 0; i < n; i++ {
-			fmt.Scan(&e, &q1, &q2)
-			result[e] += q1 * q2
+			scanner.Scan()
+			input := strings.Split(scanner.Text(), " ")
+			e, _ := strconv.Atoi(input[0])
+			p, _ := strconv.Atoi(input[1])
+			q, _ := strconv.Atoi(input[2])
+
+			if e >= 1 && e <= 4000 && memberCheck[e] == false {
+				members = append(members, e)
+				memberCheck[e] = true
+			}
+			sales[e-1] += p * q
 		}
 
 		emptyFlag := true
-		for i, v := range result {
-			if v >= 1000000 {
+		for _, v := range members {
+			if sales[v-1] >= 1000000 {
+				fmt.Println(v)
 				emptyFlag = false
-				fmt.Println(i)
 			}
 		}
 
-		if emptyFlag {
+		if emptyFlag == true {
 			fmt.Println("NA")
 		}
 	}
